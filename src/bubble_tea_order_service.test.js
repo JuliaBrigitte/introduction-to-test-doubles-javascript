4e39ce6fe3e7680aaca2a2bb8f00c9f28c12eedc
Julia Branke <Julia.Branke@gmx.co.uk>
2022-03-10 21:04:30 +0000

    Familiarised myself with test Doubles

--- src/bubble_tea_order_service.test.js
+++ src/bubble_tea_order_service.test.js
@@ -4,6 +4,7 @@ const bubbleTeaMessenger = require('./bubble_tea_messenger');
 jest.mock('./bubble_tea_messenger');
 jest.mock('./simple_logger');
 
+// dummy data
 let dummyPaymentDetails;
 
 beforeEach(() => {
@@ -40,3 +41,18 @@ test('test successful bubble tea order request', () => {
   expect(bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail)
       .toHaveBeenCalledTimes(1);
 });
+
+
+// The acceptance criteria is captured by the test function:
+//     test('test successful bubble tea order request', () => {
+// // test is in here
+// });
+// What do we need for our test?
+//     Here's a summary of what we need to do for Arrange, Act, Assert:
+//
+// Assert
+// Check that the result we get from calling the bubble_tea_order_service's createOrderRequest function produces the expected JSON response.
+// Act
+// Call the bubble_tea_order_service's createOrderRequest function with a JSON Object that represents a Bubble Tea Request
+// Arrange
+// Set up the JSON objects required to make a Bubble Tea Request that we can use for testing
--- src/bubble_tea_roulette_service.test.js
+++ src/bubble_tea_roulette_service.test.js
@@ -14,3 +14,46 @@ test('should generate random bubble tea', () => {
   // Restore the default Math.random
   jest.spyOn(global.Math, 'random').mockRestore();
 });
+
+//  PEACHICETEA
+test('should generate PEACHICETEA bubble tea', () => {
+  // Arrange
+  // Stub to calls to Math.random to return 0.2
+  jest.spyOn(global.Math, 'random').mockReturnValue(0.7);
+
+  // Act
+  const bubbleTea = getRandomBubbleTeaType();
+
+  // Assert
+  expect(bubbleTea).toBe('PEACHICETEA');
+
+  // Restore the default Math.random
+  jest.spyOn(global.Math, 'random').mockRestore();
+});
+
+//   OOLONGMILKTEA: 'OolongMilkTea',
+//   JASMINEMILKTEA: 'JasmineMilkTea',
+//   MATCHAMILKTEA: 'MatchaMilkTea',
+//   PEACHICETEA: 'PeachIceTea',
+//   LYCHEEICETEA: 'LycheeIceTea',
+
+test.each([
+  {a: .1, expected: 'OOLONGMILKTEA'},
+  {a: .3, expected: 'JASMINEMILKTEA'},
+  {a: .5, expected: 'MATCHAMILKTEA'},
+  {a: .7, expected: 'PEACHICETEA'},
+  {a: .9, expected: 'LYCHEEICETEA'},
+])('parameterised test bubble tea', ({a, expected}) => {
+  // Arrange
+  // Stub to calls to Math.random to return 0.2
+  jest.spyOn(global.Math, 'random').mockReturnValue(a);
+
+  // Act
+  const bubbleTea = getRandomBubbleTeaType();
+
+  // Assert
+  expect(bubbleTea).toBe(expected);
+
+  // Restore the default Math.random
+  jest.spyOn(global.Math, 'random').mockRestore();
+});
